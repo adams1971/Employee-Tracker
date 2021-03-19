d62be011d63ceec977930358570c1de2a2e0adab
adam stevenson <adamscdcm@gmail.com>
2021-03-19 00:21:28 -0700

    added sql folder

---
+++ sql/empTrac.sql
@@ -0,0 +1,37 @@
+-- Drops the empTrac_db; if it exists currently --
+DROP DATABASE IF EXISTS empTrac_db;
+-- Creates the "empTrac_db;" database --
+CREATE DATABASE empTrac_db;
+
+-- Makes it so all of the following code will affect empTrac_db; --
+USE empTrac_db;
+
+-- Creates table "department" --
+CREATE TABLE department (
+  id INT AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+-- Creates table "role" --
+CREATE TABLE role (
+id INT AUTO_INCREMENT NOT NULL,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL (10,4)NOT NULL,
+department_id INT,  
+PRIMARY KEY (id),
+FOREIGN KEY (department_id) REFERENCES Department (id)
+);
+
+-- Creates table "employee" --
+CREATE TABLE employee (
+id INT AUTO_INCREMENT NOT NULL,
+first_name VARCHAR(30) NULL,
+last_name VARCHAR(30) NULL,
+role_id INT,
+manager_id INT,
+PRIMARY KEY (id),
+FOREIGN KEY (role_id) REFERENCES Role (id),
+FOREIGN KEY (manager_id) REFERENCES Employee (id)
+
+);
---
+++ sql/seeds.sql
@@ -0,0 +1,33 @@
+USE empTrac_db;
+
+INSERT INTO department (name)
+VALUES ("Culinary"),
+("B&C"),
+("Maintenance");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Culinary Lead", 45000, 1),
+("Culinary Cook", 34000, 1),
+("Culinary Prep", 30000, 1),
+("B&C Lead", 40000, 2),
+("B&C Server", 20000, 2),
+("B&C House Men", 25000, 2),
+("Maintenance Grounds", 25000, 3),
+("Maintenance Electrical", 46000, 3),
+("Maintenance Plumbing", 45000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Blue", "Barry", 1),
+("Rock", "Cod", 2),
+("Steak", "Fritz", 3),
+("Lucy", "Lead", 4),
+("Sally", "Server", 5),
+("Henry", "House", 6),
+("Gery", "Grounds", 7),
+("Eric", "Electric", 8),
+("Peter", "Plumber", 9);
+
+
+
+
+
